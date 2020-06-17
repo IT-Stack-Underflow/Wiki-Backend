1368744fba0fe5fc61d5219c4556f7d4ae07e488
Michael Jeong <michael@MacBook-Pro.hitronhub.home>
2020-06-16 19:58:07 -0700

    Project Initialize

---
+++ src/main/java/com/itstackunderflow/wiki/ItStackUnderflowWikiApplication.java
@@ -0,0 +1,13 @@
+package com.itstackunderflow.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItStackUnderflowWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItStackUnderflowWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itstackunderflow/wiki/ItStackUnderflowWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itstackunderflow.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItStackUnderflowWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
